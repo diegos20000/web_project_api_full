237c80455497d964668a1a6f9364b8658a67347b
Diego Rodrigo Cano <dsuccess2020@gmail.com>
2025-05-29 16:29:06 -0500

    jwterror

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,7 +14,7 @@ const auth = async (req, res, next) => {
     const token = authHeader.replace("Bearer ", "").trim();
     console.log("Token extraído:", token);
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "secret");
     const user = await User.findById(decoded.id);
     
 
